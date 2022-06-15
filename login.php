caf0c427bda9cad988ae552e5daa189cafbfde1e
ana1864 <107557702+ana1864@users.noreply.github.com>
2022-06-15 11:01:01 -0500

    Add files via upload

---
+++ login.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="estilos2.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&family=Lato:wght@300&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600&family=Noto+Sans+JP&display=swap" rel="stylesheet"> 
+
+	
+  <title>Soporte</title>
+</head>
+
+
+<body>
+
+	
+
+  </header>
+  <section class="form-register">
+    <h4>Iniciar Sesion</h4>
+    <input class="controls" type="text" name="nombres" id="nombres" placeholder="Usuario">
+    
+    <input class="controls" type="password" name="correo" id="correo" placeholder=" Contraseña">
+
+
+   
+   <br><br>
+    <input class="bot" type="submit" value="ENTRAR"> 
+
+    <p><a href="registroUsuario.php">Registrate</a></p>
+  </section>
+  
+
+
+<style type="text/css">
+  
+.bot{
+
+padding-top: 30px;
+background-color: #272626; 
+width: 230px;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+
+
+}
+
+
+
+
+</style>
+
+  
+  </body>
+  </html>
---
+++ registroUsuario.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="estilos2.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&family=Lato:wght@300&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600&family=Noto+Sans+JP&display=swap" rel="stylesheet"> 
+
+	
+  <title>Soporte</title>
+</head>
+
+
+<body>
+
+	
+
+  </header>
+  <section class="form-register">
+    <h4>Registro</h4>
+    <input class="controls" type="text" name="nombres" id="nombres" placeholder="Ingrese su Nombre">
+    <input class="controls" type="text" name="apellidos" id="apellidos" placeholder="Ingrese su Apellido">
+    <input class="controls" type="email" name="correo" id="correo" placeholder="Ingrese su Correo">
+    <input class="controls" type="password" name="correo" id="correo" placeholder="Ingrese su Contraseña">
+   
+    <input class="botons" type="submit" value="Registrar">
+    <p><a href="login.php">Iniciar Sesion</a></p>
+  </section>
+  
+  
+  </body>
+  </html>
